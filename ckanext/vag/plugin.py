a892813e009e88fb3ec4dd06741032fa141a229b
de-vag-opendata <opendata.vag.de@hecht-software.de>
2021-04-29 12:06:05 +0200

    Changes for upgrade CKAN 2.8.x to 2.9.2 and upgrade Python 2.7 to 3.6
    - Syntax changes (tabs not allowed)
    - replace fanstatic resource by webassets
    - webassets.yml required in CKAN 2.9.2
    - Moved fonts to public as we don't know how to import fonts in CSS with webassets.yml
    - Overriding main.css didn't work anymore. Required moving to {% block custom_styles %}
    - Page 'real_time_widget' doesn't work anymore. Temporarily removed it as a workaround until we know what's wrong here.
      There seems to be a conflict with the pages extension

--- ckanext/vag/plugin.py
+++ ckanext/vag/plugin.py
@@ -13,16 +13,13 @@ class VagPlugin(plugins.SingletonPlugin, DefaultTranslation):
     # IRoutes
 
     def before_map(self, map):
-    	#Define vag routes
-    	rtw_controller = 'ckanext.vag.controllers.widget:RealTimeWidgetController'
+        #Define vag routes
+        rtw_controller = 'ckanext.vag.controllers.widget:RealTimeWidgetController'
 
-    	map.connect('real_time_widget',
-    		'/real_time_widget',
-    		 controller=rtw_controller, 
-    		 action='real_time_widget')
+        map.connect('real_time_widget', '/real_time_widget', controller=rtw_controller, action='real_time_widget')
         return map
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'vag')
+        toolkit.add_resource('assets', 'vag')
